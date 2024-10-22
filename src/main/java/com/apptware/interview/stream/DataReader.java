9d6c586afeec4f2fd79611ea66f6dfb9b46556f6
Ajit Buvaji Bachhav <118176546+AjitBachhav@users.noreply.github.com>
2024-10-22 21:18:26 +0530

    Update DataReader.java

--- src/main/java/com/apptware/interview/stream/DataReader.java
+++ src/main/java/com/apptware/interview/stream/DataReader.java
@@ -4,7 +4,18 @@ import java.util.stream.Stream;
 
 public interface DataReader {
 
-  Stream<String> fetchLimitadData(int limit);
+    /**
+     * Fetches a limited amount of data as a stream.
+     *
+     * @param limit the maximum number of items to fetch
+     * @return a stream containing up to the specified number of items
+     */
+    Stream<String> fetchLimitedData(int limit);
 
-  Stream<String> fetchFullData();
+    /**
+     * Fetches the full set of data as a stream.
+     *
+     * @return a stream containing all available data
+     */
+    Stream<String> fetchFullData();
 }
