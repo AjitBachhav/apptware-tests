5b750f1fb05793826409c1f578dcdb643d502876
Ajit Buvaji Bachhav <118176546+AjitBachhav@users.noreply.github.com>
2024-10-22 21:20:00 +0530

    Update PaginationService.java

--- src/main/java/com/apptware/interview/stream/PaginationService.java
+++ src/main/java/com/apptware/interview/stream/PaginationService.java
@@ -4,7 +4,15 @@ import java.util.List;
 
 public interface PaginationService {
 
-  int FULL_DATA_SIZE = 10000;
+    // Constant representing the full size of the data set
+    int FULL_DATA_SIZE = 10000;
 
-  List<String> getPaginatedData(int page, int pageSize);
+    /**
+     * Retrieves a paginated list of data.
+     *
+     * @param page the page number to retrieve (1-based index)
+     * @param pageSize the number of items per page
+     * @return a list containing the data for the specified page
+     */
+    List<String> getPaginatedData(int page, int pageSize);
 }
